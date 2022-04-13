9fec76bb006ef3e9c353721b0e026c13ff641084
Eya Gargah <eyagargah@hotmail.com>
2022-04-13 21:04:54 +0100

    Add script and update html and css file

---
+++ script.js
@@ -0,0 +1,36 @@
+//Selectors
+var thankYouCard = document.querySelector(".thanks-state")
+var rateCard = document.querySelector(".rating-card")
+var submitBtn = document.querySelector(".submit-btn");
+var score = document.querySelector("#score")
+const ratingBtns = document.querySelectorAll('.rating-btn');
+let scores = 1
+
+//EventListeners
+submitBtn.addEventListener("click", onSubmit)
+
+//Functions
+ratingBtns.forEach( btn => {
+  btn.addEventListener('click', rateBtnClick);
+});
+
+function onSubmit(){
+  rateCard.classList.add('hide');
+  score.textContent =scores;
+  thankYouCard.classList.remove('hide');
+  console.log('submit click');
+}
+
+function rateBtnClick(event) {
+  ratingBtns.forEach(btn => {
+      btn.classList.remove('active');
+  });
+  
+
+  if (event.target.classList.contains('rating-btn')){
+      event.target.classList.add('active');
+  } else {
+      event.target.parentElement.classList.add('active');
+  }
+  scores = event.target.textContent;
+}
